3ba6b715402c698223a5b1bd20ef5a61b85a7c18
DannyBoy <neo3nt3r@gmail.com>
2019-06-07 12:00:28 -0300

    Update myhash.py
    pyfiglet ao inves de os.system('figlet')

--- myhash.py
+++ myhash.py
@@ -1,14 +1,26 @@
-import hashlib, sys
-from string import *
-import os 
-
+import hashlib, sys, string, os, re
+import urllib3
+import urllib
+from pyfiglet import Figlet
+def decode():
+	print("Coming Soon")
 def encode():
-    a = input("Hash -> ")
+    a = input("cryptography -> ")
     a = a.lower()
     h = hashlib.new(a)
     b = input("Text -> ")
     c = b.encode('utf-8')
     h.update(b"%b" % c)
     print(h.hexdigest())
-encode()
+def main():	
+	f = Figlet(font='slant')
+	print (f.renderText('myhash'))
+	print("Encode -> 1")
+	print("Decode -> 2")
+	s = int(input(": "))
+	if s == 1:
+		encode()
+	elif s == 2:
+		decode()	
+main()
 #creditos ao capitão do discord
