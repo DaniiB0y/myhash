a4d4bb47b458854c5b956d9d714001225a8202d9
DannyBoy <neo3nt3r@gmail.com>
2020-11-07 01:06:19 -0300

    Update myhash.py

--- myhash.py
+++ myhash.py
@@ -1,38 +1,37 @@
-import os
-from selenium import webdriver
-from selenium.common import exceptions
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from test.test_import.data.unwritable import x
+import hashlib, sys, string, requests, os
+from bs4 import BeautifulSoup
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-options = webdriver.ChromeOptions()
-options.add_argument("headless")
-driver = webdriver.Chrome(PATH, chrome_options=options)
-driver.get("https://www.bauruempregos.com.br/home/vagas")
-dicionario = []
-dicfiltro = []
-dcvg = []
-pgnt = 0 
-c = 0
-key = ""
-vagas = 0 
-key = str(input("Buscar por: "))
-nv = int(input('Por quantas vagas?'))
-for i in range(0, nv):
-    vagas = driver.find_elements_by_tag_name("div")
-    vagas = driver.find_elements_by_class_name("descricao-vaga")
-    # Achando os links e os nomes das vagas
-    descricaovaga = vagas[i]
-    dcvg.append(descricaovaga)
-    descricaovaga.click()
-    vagas = driver.find_elements_by_class_name("descricao-vaga")
-    # Entrando e pegando os elementos da descricao da vaga
-    dicionario.append(vagas[0].text.lower())
-    # Adicionando em uma lista.lower()
-    href = "https://www.bauruempregos.com.br/home/vagas"
-    driver.get(href)
-    if key in dicionario[i]:
-        print(dicionario[i])
-    else:
-        print("Não encontrado")
+class myhash:
+    def encode(self, hash, string):
+        string = string.strip().lower()
+        string = string.encode('utf-8')
+        tips = hashlib.new(hash)
+        tips.update(b"%b" % string)
+        print("\n[+] ENCRYPT -> "+tips.hexdigest())
+    def decode(self, hash):
+        page = requests.get('https://hashtoolkit.com/reverse-hash/?hash={}'.format(hash))
+        soup = BeautifulSoup(page.content, 'lxml')
+        links = soup.findAll('a')
+        payload = ''
+        for link in links:
+            link_href = link.get('href')
+            if ('/generate-hash/?text=') in link_href:
+                payload += (link.get('href').split('?text=')[1]+'\n')
+            if payload:
+                try:
+                    payload = payload.split()
+                    a = list(set(payload))
+                    b = ''.join(a)
+                    print("\n[+] DECRYPT -> {}".format(b))
+                except:
+                    pass
+myclass = myhash()
+if len(sys.argv) == 3:
+    h = sys.argv[1]
+    s = sys.argv[2]
+    myclass.encode(h, s)
+elif len(sys.argv) == 2:
+    h = sys.argv[1]
+    myclass.decode(h)
+else:
+    print("\n[+] USAGE TO ENCRYPT -> python3 myhash.py {hash} {string}\n\n[+] USAGE TO DECRYPT -> python3 myhash.py {hash}")
